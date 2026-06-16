0c8ecfb0f392f2bf2df65e9919c3bc141bf8f727
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-16 16:09:58 +0800

    add:print matrix head file

---
+++ include/print_matrix.h
@@ -0,0 +1,21 @@
+/************************************************************
+@Author: HoodUSSEnterprise
+@Date: 2026-06-16 15:36:19
+@LastEditors: HoodUSSEnterprise
+@LastEditTime: 2026-06-16 15:37:28
+@FilePath: \asm_matrix\include\print_matrix.h
+@Description:print matrix
+*************************************************************/
+#ifndef PRINT_MATRIX_H
+#define PRINT_MATRIX_H
+
+#include "base_matrix.h"
+
+/***********************************************************
+@description: print matrix
+@param {MatrixInt} *m
+@return {*}
+*************************************************************/
+void print_matrix(MatrixInt *m);
+
+#endif // PRINT_MATRIX_H
