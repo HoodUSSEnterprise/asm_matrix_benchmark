bae483d2d48e4e793dead0116a556d4773e43edc
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-16 08:41:59 +0800

    add: add matrix head file

---
+++ include/add_matrix.h
@@ -0,0 +1,16 @@
+/************************************************************
+@Author: HoodUSSEnterprise
+@Date: 2026-06-15 20:50:55
+@LastEditors: HoodUSSEnterprise
+@LastEditTime: 2026-06-15 21:20:17
+@FilePath: \asm_matrix\include\add_matrix.h
+@Description: This is a header file that add matrix.
+*************************************************************/
+#ifndef ADD_MATRIX_H
+#define ADD_MATRIX_H
+
+#include "base_matrix.h"
+
+MatrixInt *add_matrix_int(MatrixInt *m1, MatrixInt *m2);
+
+#endif // ADD_MATRIX_H
