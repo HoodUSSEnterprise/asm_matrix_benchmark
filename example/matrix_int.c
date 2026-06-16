ad307dd1afc003f21b75b43549272f6221264b9e
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-16 16:44:58 +0800

    add:sub matrix test

--- example/matrix_int.c
+++ example/matrix_int.c
@@ -2,7 +2,7 @@
 @Author: HoodUSSEnterprise
 @Date: 2026-06-15 21:56:03
 @LastEditors: HoodUSSEnterprise
-@LastEditTime: 2026-06-16 16:21:18
+@LastEditTime: 2026-06-16 16:43:20
 @FilePath: \asm_matrix_benchmark\example\matrix_int.c
 @Description:example of matrix int
 *************************************************************/
@@ -15,8 +15,16 @@ int main(void)
     MatrixInt v1 = {data, 2, 2};
     MatrixInt v2 = {data, 1, 4};
     MatrixInt v3 = {data, 2, 2};
+    // add matrix example
+    puts("---------------------------------------add matrix---------------------------------------");
     MatrixInt *v = add_matrix_int(&v1, &v2);
     MatrixInt *m = add_matrix_int(&v1, &v3);
+    print_matrix(v);
+    print_matrix(m);
+    puts("----------------------------------------------------------------------------------------");
+    puts("---------------------------------------sub matrix---------------------------------------");
+    v = sub_matrix_int(&v1, &v2);
+    m = sub_matrix_int(&v1, &v3);
     print_matrix(v);
     print_matrix(m);
     return 0;
