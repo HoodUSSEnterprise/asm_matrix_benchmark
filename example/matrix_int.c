8eb62581427f1a35d22ec7e53cb0915d91e1e89e
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-16 17:19:32 +0800

    add:mul matrix test

--- example/matrix_int.c
+++ example/matrix_int.c
@@ -2,7 +2,7 @@
 @Author: HoodUSSEnterprise
 @Date: 2026-06-15 21:56:03
 @LastEditors: HoodUSSEnterprise
-@LastEditTime: 2026-06-16 16:43:20
+@LastEditTime: 2026-06-16 17:16:35
 @FilePath: \asm_matrix_benchmark\example\matrix_int.c
 @Description:example of matrix int
 *************************************************************/
@@ -26,6 +26,13 @@ int main(void)
     v = sub_matrix_int(&v1, &v2);
     m = sub_matrix_int(&v1, &v3);
     print_matrix(v);
+    print_matrix(m);
+    puts("----------------------------------------------------------------------------------------");
+    puts("---------------------------------------mul matrix---------------------------------------");
+    int mul_data[3] = {1, 2, 3};
+    MatrixInt mul1 = {mul_data, 3, 1};
+    MatrixInt mul2 = {mul_data, 1, 3};
+    m = mul_matrix_int(&mul1, &mul2);
     print_matrix(m);
     return 0;
 }
