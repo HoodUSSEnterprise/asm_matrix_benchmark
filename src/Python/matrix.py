a1527a546ee48b185f89393340c98016e7de1143
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-16 15:29:10 +0800

    add:replace

--- src/Python/matrix.py
+++ src/Python/matrix.py
@@ -2,7 +2,7 @@
 # @Author: HoodUSSEnterprise
 # @Date: 2026-06-16 09:16:25
 # @LastEditors: HoodUSSEnterprise
-# @LastEditTime: 2026-06-16 14:25:54
+# @LastEditTime: 2026-06-16 15:00:24
 # @FilePath: \asm_matrix\src\Python\matrix.py
 # @Description: Python file of matrix
 ###########################################################
@@ -12,11 +12,26 @@ from typing import Any, List, Optional, Union
 
 class Matrix:
 
+    ###########################################################
+    # @description: init matrix
+    # @param {*} self
+    # @param {int} rows
+    # @param {int} cols
+    # @param {List} data
+    # @param {*} float
+    # @return {*}
+    ###########################################################
     def __init__(self, rows: int, cols: int, data: List[Union[int, float]]):
         self.rows = rows
         self.cols = cols
-        self.data = data
+        self.data = data  # we use 1-dim array to save data
 
+    ###########################################################
+    # @description: opreator +
+    # @param {*} self
+    # @param {*} other
+    # @return {*}
+    ###########################################################
     def __add__(self, other: "Matrix | None") -> Optional["Matrix"]:
         if other is None:
             print("Invalid param")
@@ -32,6 +47,12 @@ class Matrix:
 
         return Matrix(self.rows, self.cols, result)
 
+    ###########################################################
+    # @description: opreator -
+    # @param {*} self
+    # @param {*} other
+    # @return {*}
+    ###########################################################
     def __sub__(self, other: "Matrix | None") -> Optional["Matrix"]:
         if other is None:
             print("Invalid param")
@@ -49,6 +70,12 @@ class Matrix:
 
         return Matrix(self.rows, self.cols, result)
 
+    ###########################################################
+    # @description: opreator *
+    # @param {*} self
+    # @param {*} other
+    # @return {*}
+    ###########################################################
     def __mul__(self, other: "Matrix | None") -> Optional["Matrix"]:
         if other is None:
             print("Invalid param")
@@ -71,6 +98,12 @@ class Matrix:
 
         return Matrix(self.rows, other.cols, result)
 
+    ###########################################################
+    # @description: opreator =
+    # @param {*} self
+    # @param {object} other
+    # @return {*}
+    ###########################################################
     def __eq__(self, other: object) -> bool:
         if other is None or not isinstance(other, Matrix):
             print("Invalid param")
@@ -89,6 +122,11 @@ class Matrix:
 
         return True
 
+    ###########################################################
+    # @description: print matrix
+    # @param {*} self
+    # @return {*}
+    ###########################################################
     def __str__(self: "Matrix | None"):
         if self is None:
             return "This matrix is None\n"
@@ -100,6 +138,12 @@ class Matrix:
                 f"--------------------------------------------"
             )
 
+    ###########################################################
+    # @description: opreator +=
+    # @param {*} self
+    # @param {*} other
+    # @return {*}
+    ###########################################################
     def __iadd__(self, other: "Matrix | None"):
         if other is None:
             print("Invalid param")
@@ -115,6 +159,12 @@ class Matrix:
 
         return self
 
+    ###########################################################
+    # @description: opreator -=
+    # @param {*} self
+    # @param {*} other
+    # @return {*}
+    ###########################################################
     def __isub__(self, other: "Matrix | None"):
         if other is None:
             print("Invalid param")
@@ -130,6 +180,12 @@ class Matrix:
 
         return self
 
+    ###########################################################
+    # @description: opreator *=
+    # @param {*} self
+    # @param {*} other
+    # @return {*}
+    ###########################################################
     def __imul__(self, other: "Matrix | None"):
         if other is None:
             print("Invalid param")
@@ -155,14 +211,32 @@ class Matrix:
 
         return self
 
+    ###########################################################
+    # @description: transport of a matrix
+    # @param {*} self
+    # @return {*}
+    ###########################################################
+    def transport(self):
+        row = self.cols
+        col = self.rows
+        result: List[Union[int, float]] = [0 for _ in range(row * col)]
+
+        for i in range(row):
+            for j in range(col):
+                result[i * col + j] = self.data[j * self.cols + i]
+
+        self.data = result
+        self.rows = col
+        self.cols = row
+
 
 ###########################################################
-# @description: Find elem in matrix
-# @param {Matrix} m
-# @param {Number} v
+# @description: find elem in matrix
+# @param {Any} m
+# @param {Union} v
 # @return {*}
 ###########################################################
-def find_elem(m: Any, v: Union[int, float]):
+def find_elem(m: Any, v: Union[int, float]) -> tuple[int, int]:
     if m is None or not isinstance(m, Matrix):
         print("Invalid param")
         return -1, -1
@@ -174,6 +248,42 @@ def find_elem(m: Any, v: Union[int, float]):
 
     print(f"No find elem {v}")
     return -1, -1
+
+
+###########################################################
+# @description: replace the specific position value
+# @param {*} m
+# @param {int} x_label : row index
+# @param {int} y_label : col index
+# @param {Union} new_value
+# @return {*}
+###########################################################
+def replace_pos(
+    m: "Matrix", x_label: int, y_label: int, new_value: Union[int, float]
+) -> None:
+    if x_label < 0 or x_label > m.cols - 1 or y_label < 0 or y_label > m.rows - 1:
+        print(f"Invalid coord ({x_label}, {y_label})")
+        return
+
+    m.data[x_label * m.cols + y_label] = new_value
+
+
+###########################################################
+# @description:
+# @param {*} m
+# @param {Union} old_value
+# @param {Union} new_value
+# @return {*}
+###########################################################
+def replace_elem(
+    m: "Matrix", old_value: Union[int, float], new_value: Union[int, float]
+) -> None:
+    if find_elem(m, old_value) != (-1, -1):
+        print(f"No find number {old_value}")
+        return
+    while find_elem(m, old_value) != (-1, -1):
+        x, y = find_elem(m, old_value)
+        m.data[x * m.cols + y] = new_value
 
 
 if __name__ == "__main__":
