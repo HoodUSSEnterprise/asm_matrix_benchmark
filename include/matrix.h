034219113269766a5628cc5126c2c7aea73aca72
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-16 08:42:15 +0800

    add:matrix head file

---
+++ include/matrix.h
@@ -0,0 +1,14 @@
+/************************************************************
+@Author: HoodUSSEnterprise
+@Date: 2026-06-15 20:51:29
+@FilePath: \asm_matrix\include\matrix.h
+@Description: example of matrix int
+*************************************************************/
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include "add_matrix.h"
+#include "base_matrix.h"
+
+#endif // MATRIX_H
