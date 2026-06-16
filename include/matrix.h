055cd6443c7c217cf77698121e25a37669e4f3c3
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-16 16:11:08 +0800

    add: new file

--- include/matrix.h
+++ include/matrix.h
@@ -1,8 +1,10 @@
 /************************************************************
 @Author: HoodUSSEnterprise
 @Date: 2026-06-15 20:51:29
+@LastEditors: HoodUSSEnterprise
+@LastEditTime: 2026-06-16 15:39:04
 @FilePath: \asm_matrix\include\matrix.h
-@Description: example of matrix int
+@Description:This is a header file that consolidates all matrix operations.
 *************************************************************/
 
 #ifndef MATRIX_H
@@ -10,5 +12,6 @@
 
 #include "add_matrix.h"
 #include "base_matrix.h"
+#include "print_matrix.h"
 
 #endif // MATRIX_H
