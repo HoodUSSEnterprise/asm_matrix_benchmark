c705ed1021c79bc3a096fe038748bfe01209a3ce
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-17 14:17:07 +0800

    add:matrix replace by value nasm code on windows

---
+++ src/assembly/windows/replace_matrix_int_by_value.asm
@@ -0,0 +1,107 @@
+;-------------------------------------------------------------
+; @Author: HoodUSSEnterprise
+; @Date: 2026-06-17 13:48:54
+; @LastEditors: HoodUSSEnterprise
+; @LastEditTime: 2026-06-17 14:04:34
+; @FilePath: \asm_matrix_benchmark\src\assembly\windows\replace_matrix_int_by_value.asm
+; @Description: replace matrix int by value nasm code on windows
+;-------------------------------------------------------------
+global replace_matrix_int_by_value
+
+; bool find_elem_int(MatrixInt *m, int elem, Point *pos);
+; rcx = m, edx = elem, r8 = pos
+extern find_elem_int
+extern puts
+
+section .rodata
+    invalid_param db "Invalid param!", 0                                   ; invalid param msg
+
+section .text
+
+; bool replace_matrix_int_by_value(MatrixInt *m, int old_data, int new_data);
+; rcx = m, edx = old_data, r8d = new_data
+replace_matrix_int_by_value:
+    ; save callee_register
+    push rbx
+    push rdi
+    push r12
+    push r13
+    push r14
+    push r15
+    sub rsp, 32 ; allocate shadow space for puts
+
+    mov r14, rcx ; r14 = m
+    mov r15d, edx ; r15d = old_data
+    mov r13d, r8d ; r13d = new_data
+
+    ; check m and pos
+    test r14, r14
+    jz null_ptr
+    test r15, r15
+    jz null_ptr
+
+    mov r14, [rcx] ; r14 = m->data
+
+    ; check m->data
+    test r14, r14
+    jz null_ptr
+
+    ; restore r14
+    mov r14, rcx
+
+    mov rcx, r14
+    mov edx, r15d
+    mov r8, 0
+    call find_elem_int
+    cmp rax, 0
+    jmp no_find
+
+    mov rsi, [r14] ; rsi = m->data
+    ; calc index of array
+    mov r10, [r14 + 16] ; r10 = m->cols
+    imul r10, [r8] ; pos.x * m->cols
+    add r10, [r8 + 8] ; pos.x * m->cols + pos.y
+
+    mov [rsi + r10 * 4], r13d ; m->data[pos.x * m->cols + pos.y] = new_data
+
+loop_replace:
+    mov rcx, r14
+    mov edx, r15d
+    mov r8, 0
+    call find_elem_int
+    cmp rax, 1
+    je replace_data
+    jne end
+
+replace_data:
+    ; calc index of array
+    mov r10, [r14 + 16] ; r10 = m->cols
+    imul r10, [r8] ; pos.x * m->cols
+    add r10, [r8 + 8] ; pos.x * m->cols + pos.y
+
+    mov [rsi + r10 * 4], r13d ; m->data[pos.x * m->cols + pos.y] = new_data
+    jmp loop_replace
+
+null_ptr:
+    lea rcx, [rel invalid_param] ; rcx = invalid_param
+    call puts
+    mov rax, 0 ; return false
+    jmp cleanup
+
+no_find:
+    mov rax, 0
+    jmp cleanup
+
+end:
+    mov rax, 1
+
+cleanup:
+    add rsp, 32 ; restore stack pointer
+    ; restore callee_register
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rdi
+    pop rbx
+    ret
