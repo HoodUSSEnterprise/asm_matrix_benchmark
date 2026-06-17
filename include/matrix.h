32b77a3437942f202f5b83aebef31711b58a5401
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-17 15:49:14 +0800

    add:new file

--- include/matrix.h
+++ include/matrix.h
@@ -2,7 +2,7 @@
 @Author: HoodUSSEnterprise
 @Date: 2026-06-15 20:51:29
 @LastEditors: HoodUSSEnterprise
-@LastEditTime: 2026-06-17 14:07:27
+@LastEditTime: 2026-06-17 15:42:32
 @FilePath: \asm_matrix_benchmark\include\matrix.h
 @Description:This is a header file that consolidates all matrix operations.
 *************************************************************/
@@ -19,5 +19,6 @@
 #include "replace_matrix.h"
 #include "scale_matrix.h"
 #include "sub_matrix.h"
+#include "transpose_matrix.h"
 
 #endif // MATRIX_H
