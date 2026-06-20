6310a82b0388f1adf0297c64c77a4039016efe96
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-20 17:18:48 +0800

    fix:rdi and rsi are not callee_register in linux

--- src/assembly/linux/sub_matrix_int.asm
+++ src/assembly/linux/sub_matrix_int.asm
@@ -25,7 +25,6 @@ sub_matrix_int:
 
     ; save callee_register
     push rbx
-    push rdi
     push r12
     push r13
     push r14
@@ -153,6 +152,5 @@ cleanup:
     pop r14
     pop r13
     pop r12
-    pop rdi
     pop rbx
     ret
