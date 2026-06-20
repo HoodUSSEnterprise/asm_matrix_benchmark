11dbe87732cef317d07a0a6107293f99671e3f03
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-20 17:18:46 +0800

    fix:rdi and rsi are not callee_register in linux

--- src/assembly/linux/compare_matrix_int.asm
+++ src/assembly/linux/compare_matrix_int.asm
@@ -24,7 +24,6 @@ is_equal_matrix_int:
 
     ; save callee_register
     push rbx
-    push rdi
     push r12
     push r13
     push r14
@@ -119,6 +118,5 @@ cleanup:
     pop r14
     pop r13
     pop r12
-    pop rdi
     pop rbx
     ret
