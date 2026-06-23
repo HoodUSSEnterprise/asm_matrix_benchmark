acb14aa87fe2929239948a51fddcec27fdae7d66
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-23 09:23:27 +0800

    fix

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,39 @@
-# package marker for src
+###########################################################
+# @Author: HoodUSSEnterprise
+# @Date: 2026-06-16 20:26:41
+# @LastEditors: HoodUSSEnterprise
+# @LastEditTime: 2026-06-22 14:07:38
+# @FilePath: \asm_matrix_benchmark\src\__init__.py
+# @Description: init
+###########################################################
+
+from .Python.base_matrix import Matrix
+from .Python.concat_matrix import cat_matrix
+from .Python.extract_matrix import extract_col, extract_row, extract_submatrix
+from .Python.find_matrix import find_elem
+from .Python.leading_minors import leading_minors
+from .Python.rank_matrix import rank
+from .Python.replace_matrix import replace_elem, replace_pos
+from .Python.random_matrix import random_matrix
+from .Python.special_matrix import identity, ones, zeros
+from .Python.trace_matrix import trace
+from .Python.transpose_matrix import transpose
+
+__all__ = [
+    "Matrix",
+    "cat_matrix",
+    "extract_col",
+    "extract_row",
+    "extract_submatrix",
+    "find_elem",
+    "identity",
+    "leading_minors",
+    "ones",
+    "random_matrix",
+    "rank",
+    "replace_elem",
+    "replace_pos",
+    "trace",
+    "transpose",
+    "zeros",
+]
