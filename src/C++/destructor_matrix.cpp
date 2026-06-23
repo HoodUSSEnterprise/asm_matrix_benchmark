a00f6d46c3cae2db79a3b30da5ab07f2a712f0b8
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-23 19:24:05 +0800

    fix: Refactor MatrixInt destructor to remove cleanup method for clarity

--- src/C++/destructor_matrix.cpp
+++ src/C++/destructor_matrix.cpp
@@ -2,18 +2,13 @@
 @Author: HoodUSSEnterprise
 @Date: 2026-06-16 19:38:27
 @LastEditors: HoodUSSEnterprise
-@LastEditTime: 2026-06-16 19:40:03
+@LastEditTime: 2026-06-23 17:38:27
 @FilePath: \asm_matrix_benchmark\src\C++\destructor_matrix.cpp
 @Description: destructor of matrix
 *************************************************************/
 #include "matrix_cpp.h"
 
 MatrixInt::~MatrixInt()
-{
-    cleanup();
-}
-
-void MatrixInt::cleanup()
 {
     data.clear();
     rows = 0;
