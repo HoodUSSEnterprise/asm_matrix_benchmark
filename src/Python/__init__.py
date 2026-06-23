d87dbf0e826c01ebfec75e4c4d0a08ecac84cf2a
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-23 17:20:20 +0800

    fupdate new file

--- src/Python/__init__.py
+++ src/Python/__init__.py
@@ -1,19 +1,26 @@
 ###########################################################
 # @Author: HoodUSSEnterprise
-# @Date: 2026-06-22 13:14:32
+# @Date: 2026-06-23 17:15:27
 # @LastEditors: HoodUSSEnterprise
-# @LastEditTime: 2026-06-22 13:17:24
+# @LastEditTime: 2026-06-23 17:18:51
 # @FilePath: \asm_matrix_benchmark\src\Python\__init__.py
 # @Description: init file
 ###########################################################
+
 from .base_matrix import Matrix
 from .concat_matrix import cat_matrix
 from .extract_matrix import extract_col, extract_row, extract_submatrix
 from .find_matrix import find_elem
-from .leading_minors import leading_minors
+from .leading_minors import determinant, leading_minors, principal_minor
 from .rank_matrix import rank
 from .replace_matrix import replace_elem, replace_pos
-from .random_matrix import random_matrix
+from .random_matrix import (
+    random_diagonal,
+    random_float,
+    random_integer,
+    random_matrix,
+    random_symmetric,
+)
 from .special_matrix import identity, ones, zeros
 from .trace_matrix import trace
 from .transpose_matrix import transpose
@@ -21,6 +28,7 @@ from .transpose_matrix import transpose
 __all__ = [
     "Matrix",
     "cat_matrix",
+    "determinant",
     "extract_col",
     "extract_row",
     "extract_submatrix",
@@ -28,7 +36,12 @@ __all__ = [
     "identity",
     "leading_minors",
     "ones",
+    "principal_minor",
+    "random_diagonal",
+    "random_float",
+    "random_integer",
     "random_matrix",
+    "random_symmetric",
     "rank",
     "replace_elem",
     "replace_pos",
