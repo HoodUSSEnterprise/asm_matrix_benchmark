085dc0308af468731b18fdea23ebac85475f9f93
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-23 19:21:30 +0800

    fix: Refactor print matrix function to use ostream operator for better integration

--- src/C++/print_matrix.cpp
+++ src/C++/print_matrix.cpp
@@ -2,31 +2,32 @@
 @Author: HoodUSSEnterprise
 @Date: 2026-06-16 19:45:21
 @LastEditors: HoodUSSEnterprise
-@LastEditTime: 2026-06-16 19:56:00
+@LastEditTime: 2026-06-23 19:21:21
 @FilePath: \asm_matrix_benchmark\src\C++\print_matrix.cpp
 @Description: print matrix c++ code
 *************************************************************/
 #include "matrix_cpp.h"
 
 /***********************************************************
-@description:  print matrix function
+@description: print matrix with operator <<
 @return {*}
- ************************************************************/
-void MatrixInt::print_matrix()
+*************************************************************/
+std::ostream &operator<<(std::ostream &os, const MatrixInt &m)
 {
-    std::cout << "------------------matrix info------------------" << std::endl;
-    std::cout << "matrix size: (" << this->rows << "," << this->cols << ")" << std::endl;
-    std::cout << "matrix data:" << std::endl;
-    for (size_t i = 0; i < this->rows; ++i)
+    os << "--------------------------------------------" << std::endl;
+    os << "matrix size: (" << m.rows << ", " << m.cols << ")" << std::endl;
+    os << "matrix data:" << std::endl;
+    for (size_t i = 0; i < m.rows; ++i)
     {
-        for (size_t j = 0; j < this->cols; ++j)
+        for (size_t j = 0; j < m.cols; ++j)
         {
-            std::cout << this->data[i * this->cols + j];
-            if (j + 1 < this->cols)
+            os << m.data[i * m.cols + j];
+            if (j + 1 < m.cols)
             {
-                std::cout << ' ';
+                os << ' ';
             }
         }
-        std::cout << std::endl;
+        os << std::endl;
     }
+    return os;
 }
