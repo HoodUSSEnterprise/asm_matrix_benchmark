3dfd3f62e40e25c351d1d970c2cec2232236e8bd
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-23 19:48:59 +0800

    feat: Update CMakeLists.txt and sample code, add matrix operation examples and new matrix functions

--- example/matrix_int.cpp
+++ example/matrix_int.cpp
@@ -1,8 +1,8 @@
 /************************************************************
 @Author: HoodUSSEnterprise
-@Date: 2026-06-16 19:46:42
+@Date: 2026-06-23 17:46:42
 @LastEditors: HoodUSSEnterprise
-@LastEditTime: 2026-06-16 19:53:30
+@LastEditTime: 2026-06-23 19:47:32
 @FilePath: \asm_matrix_benchmark\example\matrix_int.cpp
 @Description:example of matrix int c++
 *************************************************************/
@@ -19,29 +19,135 @@ int main()
 
     // add matrix example
     puts("---------------------------------------add matrix---------------------------------------");
-    MatrixInt m = v1.add_matrix(v1, v3);
-    m.print_matrix();
+    MatrixInt m = v1 + v3;
+    std::cout << m;
     puts("----------------------------------------------------------------------------------------");
 
     puts("---------------------------------------sub matrix---------------------------------------");
-    m = v1.sub_matrix(v1, v3);
-    m.print_matrix();
+    m = v1 - v3;
+    std::cout << m;
     puts("----------------------------------------------------------------------------------------");
 
     puts("---------------------------------------mul matrix---------------------------------------");
     int mul_data[3] = {1, 2, 3};
     MatrixInt mul1(mul_data, 3, 1);
     MatrixInt mul2(mul_data, 1, 3);
-    m = mul1.mul_matrix(mul1, mul2);
-    m.print_matrix();
+    m = mul1 * mul2;
+    std::cout << m;
     puts("----------------------------------------------------------------------------------------");
 
     puts("--------------------------------------scale matrix--------------------------------------");
     int scale_data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     MatrixInt scale(scale_data, 2, 5);
-    scale.print_matrix();
-    m = scale.scale_matrix(scale, 2);
-    m.print_matrix();
+    std::cout << scale;
+    m = scale * 2;
+    std::cout << m;
+    puts("----------------------------------------------------------------------------------------");
+
+    puts("---------------------------------------cat matrix---------------------------------------");
+    int cat_data1[3] = {1, 2, 3};
+    int cat_data2[6] = {4, 5, 6, 7, 8, 9};
+    MatrixInt cat1(cat_data1, 1, 3);
+    MatrixInt cat2(cat_data2, 2, 3);
+    m = cat1.cat(cat2, 0);
+    std::cout << m;
+    puts("----------------------------------------------------------------------------------------");
+
+    puts("--------------------------------------find matrix---------------------------------------");
+    int find_data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    MatrixInt find_m(find_data, 5, 2);
+    std::cout << find_m;
+    auto p = find_m.find_elem(5);
+    if (p.first != -1)
+    {
+        printf("find elem 5 at (%d, %d)\n", p.first, p.second);
+    }
+    else
+    {
+        printf("No find elem 5\n");
+    }
+    p = find_m.find_elem(11);
+    if (p.first != -1)
+    {
+        printf("find elem 11 at (%d, %d)\n", p.first, p.second);
+    }
+    else
+    {
+        printf("No find elem 11\n");
+    }
+    puts("----------------------------------------------------------------------------------------");
+
+    puts("-------------------------------------replace matrix-------------------------------------");
+    int replace_data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    MatrixInt replace_m(replace_data, 5, 2);
+    replace_m.replace_pos(0, 0, 99);
+    std::cout << replace_m;
+    replace_m.replace_elem(7, 77);
+    std::cout << replace_m;
+    puts("----------------------------------------------------------------------------------------");
+
+    puts("------------------------------------transpose matrix------------------------------------");
+    int transpose_data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    MatrixInt raw(transpose_data, 5, 2);
+    std::cout << raw;
+    MatrixInt t = raw.transpose();
+    std::cout << t;
+    puts("----------------------------------------------------------------------------------------");
+
+    puts("-------------------------------------special matrix-------------------------------------");
+    MatrixInt I = MatrixInt::identity(4);
+    std::cout << I;
+    MatrixInt Z = MatrixInt::zeros(3, 4);
+    std::cout << Z;
+    MatrixInt O = MatrixInt::ones(2, 3);
+    std::cout << O;
+    MatrixInt R = MatrixInt::random(3, 3, 0, 10);
+    std::cout << R;
+    puts("----------------------------------------------------------------------------------------");
+
+    puts("-------------------------------------rank and trace-------------------------------------");
+    int rt_data[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    MatrixInt rt_m(rt_data, 3, 3);
+    std::cout << rt_m;
+    printf("matrix rank = %zu\n", rt_m.rank());
+    printf("matrix trace = %d\n", rt_m.trace());
+    puts("----------------------------------------------------------------------------------------");
+
+    puts("-------------------------------------compare matrix-------------------------------------");
+    int comp1_data[6] = {1, 2, 3, 4, 5, 6};
+    int comp2_data[6] = {6, 5, 4, 3, 2, 1};
+    MatrixInt cmp1(comp1_data, 2, 3);
+    MatrixInt cmp2(comp1_data, 2, 3);
+    MatrixInt cmp3(comp2_data, 2, 3);
+    std::cout << (cmp1 == cmp2 ? "Two matrix equals\n" : "Two matrix not equals\n");
+    std::cout << (cmp1 == cmp3 ? "Two matrix equals\n" : "Two matrix not equals\n");
+    std::cout << (cmp1 != cmp3 ? "Two matrix not equals\n" : "Two matrix equals\n");
+    puts("----------------------------------------------------------------------------------------");
+
+    puts("-------------------------------------determinant and leading minors---------------------");
+    int lead_data[9] = {1, 2, 3, 4, 5, 6, 7, 8, 10};
+    MatrixInt lead_m(lead_data, 3, 3);
+    std::cout << lead_m;
+    auto minors = lead_m.leading_minors();
+    printf("leading minors count = %zu\n", minors.size());
+    for (size_t i = 0; i < minors.size(); i++)
+    {
+        printf("--- order %zu ---\n", i + 1);
+        std::cout << minors[i];
+    }
+    puts("----------------------------------------------------------------------------------------");
+
+    puts("-------------------------------------extract submatrix----------------------------------");
+    int ext_data[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    MatrixInt ext_m(ext_data, 3, 4);
+    std::cout << ext_m;
+    MatrixInt row = ext_m.extract_row(1);
+    std::cout << row;
+    MatrixInt col = ext_m.extract_col(2);
+    std::cout << col;
+    MatrixInt block = ext_m.extract_submatrix(0, 2, 1, 3);
+    std::cout << block;
+    puts("----------------------------------------------------------------------------------------");
 
     return 0;
 }
--- include/matrix_cpp.h
+++ include/matrix_cpp.h
@@ -2,14 +2,14 @@
 @Author: HoodUSSEnterprise
 @Date: 2026-06-16 19:02:24
 @LastEditors: HoodUSSEnterprise
-@LastEditTime: 2026-06-17 09:18:20
+@LastEditTime: 2026-06-23 17:45:04
 @FilePath: \asm_matrix_benchmark\include\matrix_cpp.h
 @Description: matrix C++ head file
 *************************************************************/
-#ifndef MATRIX_CPP_H
-#define MATRIX_CPP_H
+#pragma once
 
 #include <iostream>
+#include <utility>
 #include <vector>
 
 class MatrixInt
@@ -18,27 +18,70 @@ private:
     size_t rows;
     size_t cols;
     std::vector<int> data;
-    void initialize(size_t r, size_t c);
-    void cleanup();
 
 public:
+    // constructors / destructor
     MatrixInt();
     MatrixInt(size_t r, size_t c);
     MatrixInt(const int *arr, size_t r, size_t c);
     MatrixInt(const MatrixInt &other);
+    MatrixInt(MatrixInt &&other) noexcept;
     ~MatrixInt();
 
-    MatrixInt add_matrix(MatrixInt m1, MatrixInt m2);
+    // assignment
+    MatrixInt &operator=(const MatrixInt &other);
+    MatrixInt &operator=(MatrixInt &&other) noexcept;
 
-    MatrixInt sub_matrix(MatrixInt m1, MatrixInt m2);
+    // element access
+    int &operator()(size_t i, size_t j);
+    const int &operator()(size_t i, size_t j) const;
 
-    MatrixInt mul_matrix(MatrixInt m1, MatrixInt m2);
+    // getters
+    size_t get_rows() const { return rows; }
+    size_t get_cols() const { return cols; }
 
-    MatrixInt scale_matrix(MatrixInt m1, int m2);
+    // arithmetic operators
+    MatrixInt operator+(const MatrixInt &other) const;
+    MatrixInt operator-(const MatrixInt &other) const;
+    MatrixInt operator*(const MatrixInt &other) const;
+    MatrixInt operator*(int scalar) const;
+    friend MatrixInt operator*(int scalar, const MatrixInt &m);
 
-    MatrixInt cat_matrix(MatrixInt m, int axis);
+    // compound assignment
+    MatrixInt &operator+=(const MatrixInt &other);
+    MatrixInt &operator-=(const MatrixInt &other);
+    MatrixInt &operator*=(const MatrixInt &other);
+    MatrixInt &operator*=(int scalar);
 
-    void print_matrix();
+    // comparison
+    bool operator==(const MatrixInt &other) const;
+    bool operator!=(const MatrixInt &other) const;
+
+    // output
+    friend std::ostream &operator<<(std::ostream &os, const MatrixInt &m);
+
+    // matrix operations
+    MatrixInt transpose() const;
+    MatrixInt cat(const MatrixInt &other, int axis) const;
+
+    // find / replace
+    std::pair<int, int> find_elem(int value) const;
+    void replace_pos(size_t x, size_t y, int new_value);
+    void replace_elem(int old_value, int new_value);
+
+    // extract submatrix
+    MatrixInt extract_row(size_t row_idx) const;
+    MatrixInt extract_col(size_t col_idx) const;
+    MatrixInt extract_submatrix(size_t r_start, size_t r_end, size_t c_start, size_t c_end) const;
+
+    // linear algebra
+    size_t rank() const;
+    int trace() const;
+    std::vector<MatrixInt> leading_minors() const;
+
+    // static factories
+    static MatrixInt identity(size_t n);
+    static MatrixInt zeros(size_t r, size_t c);
+    static MatrixInt ones(size_t r, size_t c);
+    static MatrixInt random(size_t r, size_t c, int low = 0, int high = 10);
 };
-
-#endif // MATRIX_CPP_H
