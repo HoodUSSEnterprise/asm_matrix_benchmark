b0e9d3811ce6183c76d9e5898edd59b1f4273826
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-23 19:59:24 +0800

    feat: Add matrix addition functions for double precision in add_matrix module

--- include/add_matrix.h
+++ include/add_matrix.h
@@ -2,7 +2,7 @@
 @Author: HoodUSSEnterprise
 @Date: 2026-06-15 20:50:55
 @LastEditors: HoodUSSEnterprise
-@LastEditTime: 2026-06-20 15:39:51
+@LastEditTime: 2026-06-23 19:56:01
 @FilePath: \asm_matrix_benchmark\include\add_matrix.h
 @Description: This is a header file that add matrix.
 *************************************************************/
@@ -12,11 +12,19 @@
 #include "base_matrix.h"
 
 /***********************************************************
-@description: add matrix
+@description: add matrix int
 @param {MatrixInt} *m1
 @param {MatrixInt} *m2
 @return {*}
  ************************************************************/
 MatrixInt *add_matrix_int(MatrixInt *m1, MatrixInt *m2);
+
+/***********************************************************
+@description: add matrix double
+@param {MatrixDouble} *m1
+@param {MatrixDouble} *m2
+@return {*}
+*************************************************************/
+MatrixDouble *add_matrix_double(MatrixDouble *m1, MatrixDouble *m2);
 
 #endif // ADD_MATRIX_H
