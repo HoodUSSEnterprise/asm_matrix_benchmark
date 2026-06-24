b40ef8c880360427d526d3095627bedbf8c962fd
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-24 20:20:12 +0800

    docs: standardize header date format and description in special matrix float functions

--- src/assembly/windows/special_matrix_float.asm
+++ src/assembly/windows/special_matrix_float.asm
@@ -1,12 +1,13 @@
 ;-------------------------------------------------------------
 ; @Author: HoodUSSEnterprise
-; @Date: 2026-06-24
+; @Date: 2026-06-24 18:52:55
 ; @LastEditors: HoodUSSEnterprise
-; @LastEditTime: 2026-06-24
+; @LastEditTime: 2026-06-24 19:32:57
 ; @FilePath: \asm_matrix_benchmark\src\assembly\windows\special_matrix_float.asm
 ; @Description: some special matrix like identity matrix, diag matrix,
 ; eye_matrix and zero matrix float nasm code on windows
 ;-------------------------------------------------------------
+
 global identity_matrix_float, diag_matrix_float, eye_matrix_float, zero_matrix_float
 extern malloc
 extern free
