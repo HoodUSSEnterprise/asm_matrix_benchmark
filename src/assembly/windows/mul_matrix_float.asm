a092f56297880090ae3f38c3cef57ade25bfd189
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-24 20:20:09 +0800

    docs: standardize header date format and description in mul_matrix_float

--- src/assembly/windows/mul_matrix_float.asm
+++ src/assembly/windows/mul_matrix_float.asm
@@ -1,10 +1,10 @@
 ;-------------------------------------------------------------
 ; @Author: HoodUSSEnterprise
-; @Date: 2026-06-24
+; @Date: 2026-06-24 18:52:55
 ; @LastEditors: HoodUSSEnterprise
-; @LastEditTime: 2026-06-24
+; @LastEditTime: 2026-06-24 19:28:36
 ; @FilePath: \asm_matrix_benchmark\src\assembly\windows\mul_matrix_float.asm
-; @Description: mul matrix float nasm code on windows
+; @Description:  mul matrix float nasm code on windows
 ;-------------------------------------------------------------
 
 global mul_matrix_float
