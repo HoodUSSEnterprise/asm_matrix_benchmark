8198f8186e94ceb6dc292362982369ff15eb2459
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-24 15:41:48 +0800

    feat: implement replace_matrix_float functions

--- src/C/replace_matrix.c
+++ src/C/replace_matrix.c
@@ -78,3 +78,74 @@ bool replace_matrix_int_by_value(MatrixInt *m, int old_data, int new_data)
     }
     return true;
 }
+
+/***********************************************************
+@description: replace matrix float some data by coordinate
+@param {MatrixFloat} *m
+@param {Point} *pos
+@param {float} new_data
+@return {*}
+************************************************************/
+bool replace_matrix_float_by_coord(MatrixFloat *m, Point *pos, float new_data)
+{
+    // check m and pos
+    if (m == NULL || pos == NULL)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return false;
+    }
+    // check m->data
+    if (m->data == NULL)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return false;
+    }
+    size_t x = pos->x;
+    size_t y = pos->y;
+    // check x and y range
+    if (x < 0 || x >= m->cols || y < 0 || y >= m->rows)
+    {
+        puts("index out of range");
+        return 0;
+    }
+    // replace with new data
+    m->data[x * m->cols + y] = new_data;
+    return true;
+}
+
+/***********************************************************
+@description: replace matrix float some data by value
+@param {MatrixFloat} *m
+@param {float} old_data
+@param {float} new_data
+@return {*}
+************************************************************/
+bool replace_matrix_float_by_value(MatrixFloat *m, float old_data, float new_data)
+{
+    // check m
+    if (m == NULL)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return false;
+    }
+    // check m->data
+    if (m->data == NULL)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return false;
+    }
+    // replace value with while loop
+    Point pos = {0, 0};
+    if (find_elem_float(m, old_data, &pos) == false)
+    {
+        return false;
+    }
+    while (find_elem_float(m, old_data, &pos))
+    {
+        size_t x = pos.x;
+        size_t y = pos.y;
+        // replace with new data
+        m->data[x * m->cols + y] = new_data;
+    }
+    return true;
+}
