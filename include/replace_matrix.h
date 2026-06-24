8a8331ffb13ada64a7f04fa9ed5ff77b225e20c7
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-24 15:22:36 +0800

    feat: Add declaration of double matrix data replacement function

--- include/replace_matrix.h
+++ include/replace_matrix.h
@@ -30,4 +30,22 @@ bool replace_matrix_int_by_coord(MatrixInt *m, Point *pos, int new_data);
  ************************************************************/
 bool replace_matrix_int_by_value(MatrixInt *m, int old_data, int new_data);
 
+/***********************************************************
+@description: replace matrix double some data by coordinate
+@param {MatrixDouble} *m
+@param {Point} *pos
+@param {double} new_data
+@return {*}
+************************************************************/
+bool replace_matrix_double_by_coord(MatrixDouble *m, Point *pos, double new_data);
+
+/***********************************************************
+@description: replace matrix double some data by value
+@param {MatrixDouble} *m
+@param {double} old_data
+@param {double} new_data
+@return {*}
+************************************************************/
+bool replace_matrix_double_by_value(MatrixDouble *m, double old_data, double new_data);
+
 #endif // REPLACE_MATRIX_H
