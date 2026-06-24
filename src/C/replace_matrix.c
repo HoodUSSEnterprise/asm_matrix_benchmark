b83b5808464c6a2ee4d6b6539c870daea0ebfc40
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-24 15:24:46 +0800

    feat: Add double matrix data replacement function, allowing replacement through coordinates and numerical values

--- src/C/replace_matrix.c
+++ src/C/replace_matrix.c
@@ -78,3 +78,74 @@ bool replace_matrix_int_by_value(MatrixInt *m, int old_data, int new_data)
     }
     return true;
 }
+
+/***********************************************************
+@description: replace matrix double some data by coordinate
+@param {MatrixDouble} *m
+@param {Point} *pos
+@param {double} new_data
+@return {*}
+************************************************************/
+bool replace_matrix_double_by_coord(MatrixDouble *m, Point *pos, double new_data)
+{
+    // check m and pos
+    if (m == NULL || pos == NULL)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return false;
+    }
+    // check m->data
+    if (m->data == NULL)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return false;
+    }
+    size_t x = pos->x;
+    size_t y = pos->y;
+    // check x and y range
+    if (x < 0 || x >= m->cols || y < 0 || y >= m->rows)
+    {
+        puts("index out of range");
+        return 0;
+    }
+    // replace with new data
+    m->data[x * m->cols + y] = new_data;
+    return true;
+}
+
+/***********************************************************
+@description: replace matrix double some data by value
+@param {MatrixDouble} *m
+@param {double} old_data
+@param {double} new_data
+@return {*}
+************************************************************/
+bool replace_matrix_double_by_value(MatrixDouble *m, double old_data, double new_data)
+{
+    // check m
+    if (m == NULL)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return false;
+    }
+    // check m->data
+    if (m->data == NULL)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return false;
+    }
+    // replace value with while loop
+    Point pos = {0, 0};
+    if (find_elem_double(m, old_data, &pos) == false)
+    {
+        return false;
+    }
+    while (find_elem_double(m, old_data, &pos))
+    {
+        size_t x = pos.x;
+        size_t y = pos.y;
+        // replace with new data
+        m->data[x * m->cols + y] = new_data;
+    }
+    return true;
+}
