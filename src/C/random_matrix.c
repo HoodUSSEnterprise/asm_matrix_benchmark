6ce6d782d663de83bf0a1dc5318c0d4ec16c4597
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-24 15:24:26 +0800

    feat: Implement the function of generating random double

--- src/C/random_matrix.c
+++ src/C/random_matrix.c
@@ -87,3 +87,84 @@ MatrixInt *random_matrix_int(size_t rows, size_t cols, int *range, size_t size)
     }
     return res;
 }
+
+/***********************************************************
+@description: random matrix double
+@param {size_t} rows
+@param {size_t} cols
+@param {double} range must contains two number
+@return {*}
+************************************************************/
+MatrixDouble *random_matrix_double(size_t rows, size_t cols, double *range, size_t size)
+{
+    // check param
+    if (rows <= 0 || cols <= 0)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return NULL;
+    }
+    // check size
+    double max_boundary = 10.0;
+    double min_boundary = 0.0;
+    // default parameter
+    if (size == 0)
+    {
+        max_boundary = 10.0;
+        min_boundary = 0.0;
+    }
+    else if (size == 1)
+    {
+        if (range[0] > 0)
+        {
+            max_boundary = range[0];
+        }
+        else if (range[0] == 0)
+        {
+            max_boundary = 10.0;
+        }
+        else
+        {
+            max_boundary = 0.0;
+            min_boundary = range[0];
+        }
+    }
+    else
+    {
+        if (range[0] >= range[1])
+        {
+            max_boundary = range[0];
+            min_boundary = range[1];
+        }
+        else
+        {
+            max_boundary = range[1];
+            min_boundary = range[0];
+        }
+    }
+    // set the random number seed
+    srand((unsigned)time(NULL));
+    // malloc new res
+    MatrixDouble *res = NULL;
+    res = (MatrixDouble *)malloc(sizeof(MatrixDouble));
+    if (res == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    res->rows = rows;
+    res->cols = cols;
+    // malloc new res data
+    res->data = (double *)malloc(sizeof(double) * rows * cols);
+    if (res->data == NULL)
+    {
+        free(res);
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    for (size_t i = 0; i < rows * cols; i++)
+    {
+        double scale = (double)rand() / RAND_MAX;
+        res->data[i] = min_boundary + scale * (max_boundary - min_boundary);
+    }
+    return res;
+}
