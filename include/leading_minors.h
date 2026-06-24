a1b5da798f93c0814356fcc0eea109c0d143d812
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-24 14:54:41 +0800

    feat: add Leading_Minors_Double struct with function declaration

--- include/leading_minors.h
+++ include/leading_minors.h
@@ -2,7 +2,7 @@
 @Author: HoodUSSEnterprise
 @Date: 2026-06-21 15:42:01
 @LastEditors: HoodUSSEnterprise
-@LastEditTime: 2026-06-21 15:45:04
+@LastEditTime: 2026-06-24 14:54:39
 @FilePath: \asm_matrix_benchmark\include\leading_minors.h
 @Description: get leading principal minor
 *************************************************************/
@@ -17,11 +17,24 @@ typedef struct Leading_Minors_Int
     size_t len;
 } Leading_Minors_Int;
 
+typedef struct Leading_Minors_Double
+{
+    MatrixDouble *matrix_data;
+    size_t len;
+} Leading_Minors_Double;
+
 /***********************************************************
 @description: get leading principal minor
 @param {MatrixInt} *m
 @return {*}
  ************************************************************/
 Leading_Minors_Int *get_leading_minors_int(MatrixInt *m);
+
+/***********************************************************
+@description: get leading principal minor
+@param {MatrixInt} *m
+@return {*}
+*************************************************************/
+Leading_Minors_Double *get_leading_minors_double(MatrixDouble *m);
 
 #endif // LEADING_MINORS_H
