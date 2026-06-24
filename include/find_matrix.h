ebf74232f46726c04f2a139cccbbb21a785659e7
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-24 14:39:16 +0800

    docs: Update LastEditTime and add documentation for find_elem_double function

--- include/find_matrix.h
+++ include/find_matrix.h
@@ -2,7 +2,7 @@
 @Author: HoodUSSEnterprise
 @Date: 2026-06-17 10:12:40
 @LastEditors: HoodUSSEnterprise
-@LastEditTime: 2026-06-17 10:50:22
+@LastEditTime: 2026-06-24 14:37:37
 @FilePath: \asm_matrix_benchmark\include\find_matrix.h
 @Description: find elem position in matrix
 *************************************************************/
@@ -26,5 +26,14 @@ typedef struct Point
 @return {*}
  ************************************************************/
 bool find_elem_int(MatrixInt *m, int elem, Point *pos);
+
+/***********************************************************
+@description: find elem position in matrix m
+@param {MatrixDouble} *m
+@param {double} elem
+@param {Point} *pos
+@return {*}
+*************************************************************/
+bool find_elem_double(MatrixDouble *m, double elem, Point *pos);
 
 #endif // FIND_MATRIX_H
