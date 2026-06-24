1cab54cdf3ec90248d092c0d82f4d7438a4ee60d
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-24 15:41:50 +0800

    feat: implement transpose_matrix_float

--- src/C/transpose_matrix.c
+++ src/C/transpose_matrix.c
@@ -55,3 +55,51 @@ MatrixInt *transpose_matrix_int(MatrixInt *m)
     }
     return res;
 }
+
+/***********************************************************
+@description: transpose the matrix float
+@param {MatrixFloat} *m
+@return {*}
+************************************************************/
+MatrixFloat *transpose_matrix_float(MatrixFloat *m)
+{
+    // check m
+    if (m == NULL)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return NULL;
+    }
+    // check m->data
+    if (m->data == NULL)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return NULL;
+    }
+    // malloc res matrix
+    MatrixFloat *res = NULL;
+    res = (MatrixFloat *)malloc(sizeof(MatrixFloat));
+    if (res == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    res->rows = m->cols;
+    res->cols = m->rows;
+    // malloc for res->data
+    res->data = (float *)malloc(sizeof(float) * res->rows * res->cols);
+    if (res->data == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    // transpose means res->data[i][j] = m->data[j][i];
+    // that is res->data[i * res->cols + j] = m->data[j * m->cols + i]
+    for (size_t i = 0; i < res->rows; i++)
+    {
+        for (size_t j = 0; j < res->cols; j++)
+        {
+            res->data[i * res->cols + j] = m->data[j * m->cols + i];
+        }
+    }
+    return res;
+}
