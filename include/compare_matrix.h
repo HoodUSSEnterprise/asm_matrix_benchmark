8d858e9699a9080ec57ef266dd1e7f3d8b48f754
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-24 09:00:26 +0800

    feat: Update compare_matrix.h with double matrix comparison function

--- include/compare_matrix.h
+++ include/compare_matrix.h
@@ -2,7 +2,7 @@
 @Author: HoodUSSEnterprise
 @Date: 2026-06-19 11:07:59
 @LastEditors: HoodUSSEnterprise
-@LastEditTime: 2026-06-19 11:10:44
+@LastEditTime: 2026-06-24 08:53:54
 @FilePath: \asm_matrix_benchmark\include\compare_matrix.h
 @Description: compare two matrix
 *************************************************************/
@@ -12,6 +12,8 @@
 #include "base_matrix.h"
 #include <stdbool.h>
 
+extern const double eps;
+
 /***********************************************************
 @description: compare two matrix both int type
 @param {MatrixInt} *m1
@@ -19,5 +21,13 @@
 @return {*}
  ************************************************************/
 bool is_equal_matrix_int(MatrixInt *m1, MatrixInt *m2);
+
+/***********************************************************
+@description: compare two matrix both double type
+@param {MatrixDouble} *m1
+@param {MatrixDouble} *m2
+@return {*}
+*************************************************************/
+bool is_equal_matrix_double(MatrixDouble *m1, MatrixDouble *m2);
 
 #endif // COMPARE_MATRIX_H
