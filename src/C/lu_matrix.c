4f2231544370bd9ea2889aec929660f6a2240da5
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-24 15:49:00 +0800

    feat: implement lu_matrix_double

--- src/C/lu_matrix.c
+++ src/C/lu_matrix.c
@@ -194,3 +194,164 @@ bool LU_Decomposition_int(MatrixInt *m, LU_Result *res)
     res->U = U;
     return true;
 }
+
+/***********************************************************
+@description: lu decomposition of matrix double
+@param {MatrixDouble} *m
+@param {LU_Result} *res
+@return {*}
+************************************************************/
+bool LU_Decomposition_double(MatrixDouble *m, LU_Result *res)
+{
+    // check m and m->data
+    if (m == NULL || m->data == NULL)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return false;
+    }
+    // check dimension
+    if (m->cols != m->rows)
+    {
+        puts("It's not a square");
+        return false;
+    }
+    // check the matrix can lu decomposition or not
+    Leading_Minors_Double *leading_minors = get_leading_minors_double(m);
+    if (leading_minors == NULL)
+    {
+        fprintf(stderr, "function get_leading_minors_double has a wrong value\n");
+        return NULL;
+    }
+    for (size_t i = 0; i < leading_minors->len; i++)
+    {
+        int rank = 0;
+        if (rank_matrix_double(leading_minors->matrix_data + i, &rank) == false)
+        {
+            return false;
+        }
+        else
+        {
+            if ((size_t)rank != i + 1)
+            {
+                fprintf(stderr, "This matrix can't lu decomposition\n");
+                return false;
+            }
+        }
+    }
+
+    // free leading_minors
+    for (size_t i = 0; i < leading_minors->len; i++)
+    {
+        free((leading_minors->matrix_data + i)->data);
+    }
+    free(leading_minors->matrix_data);
+    free(leading_minors);
+
+    // malloc new L
+    MatrixDouble *L = NULL;
+    L = (MatrixDouble *)malloc(sizeof(MatrixDouble));
+    if (L == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        return false;
+    }
+    L->rows = m->rows;
+    L->cols = m->cols;
+    // malloc new L data
+    L->data = (double *)malloc(sizeof(double) * L->rows * L->cols);
+    if (L->data == NULL)
+    {
+        free(L);
+        fprintf(stderr, "Memory allocation failed\n");
+        return false;
+    }
+    // malloc new U
+    MatrixDouble *U = NULL;
+    U = (MatrixDouble *)malloc(sizeof(MatrixDouble));
+    if (U == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        return false;
+    }
+    U->rows = m->rows;
+    U->cols = m->cols;
+    // malloc new U data
+    U->data = (double *)malloc(sizeof(double) * U->rows * U->cols);
+    if (U->data == NULL)
+    {
+        free(U);
+        fprintf(stderr, "Memory allocation failed\n");
+        return false;
+    }
+    // malloc new res
+    if (res == NULL)
+    {
+        res = (LU_Result *)malloc(sizeof(LU_Result));
+        if (res == NULL)
+        {
+            fprintf(stderr, "Memory allocation failed\n");
+            return false;
+        }
+    }
+
+    // init L matrix upper triangle
+    for (size_t i = 0; i < L->rows; i++)
+    {
+        for (size_t j = i; j < L->cols; j++)
+        {
+            L->data[i * L->cols + j] = (i == j ? 1.0 : 0.0);
+        }
+    }
+
+    // init U matrix lower triangle
+    for (size_t i = 0; i < U->rows; i++)
+    {
+        for (size_t j = 0; j < i; j++)
+        {
+            U->data[i * U->cols + j] = 0.0;
+        }
+    }
+
+    /*
+    u_{1j} = a_{1j},  j = 1:n
+    l_{i1} = a_{i1} / u_{11},  i = 2:n
+    */
+    for (size_t i = 0; i < U->cols; i++)
+    {
+        U->data[i] = m->data[i];
+    }
+    for (size_t i = 1; i < L->rows; i++)
+    {
+        L->data[i * L->cols + 0] = m->data[i * m->cols + 0] / U->data[0 * U->cols + 0];
+    }
+
+    /*
+    u_{ij} = a_{ij} - sum_{k=1}^{i-1} l_{ik} u_{kj},  j = i:n
+    l_{ij} = (a_{ij} - sum_{k=1}^{j-1} l_{ik} u_{kj}) / u_{jj},  i = j+1:n
+    */
+    for (size_t i = 1; i < L->rows; i++)
+    {
+        for (size_t j = i; j < L->rows; j++)
+        {
+            double sum = 0;
+            for (size_t k = 0; k < i; k++)
+            {
+                sum += (L->data[i * L->cols + k] * U->data[k * U->cols + j]);
+            }
+            U->data[i * U->cols + j] = m->data[i * m->cols + j] - sum;
+        }
+        for (size_t j = i + 1; j < L->rows; j++)
+        {
+            double sum = 0;
+            for (size_t k = 0; k < j; k++)
+            {
+                sum += (L->data[j * L->cols + k] * U->data[k * U->cols + i]);
+            }
+            L->data[j * L->cols + i] = (m->data[j * m->cols + i] - sum) / U->data[i * U->cols + i];
+        }
+    }
+    // final result
+    res->L = L;
+    res->U = U;
+    return true;
+}
