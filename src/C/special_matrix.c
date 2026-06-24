feefc3e836e1b588cb3c2c69d888ca6c7d2693d6
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-24 15:41:49 +0800

    feat: implement special matrix float functions

--- src/C/special_matrix.c
+++ src/C/special_matrix.c
@@ -172,3 +172,166 @@ MatrixInt *zero_matrix_int(int rows, int cols)
     memset(res->data, 0, sizeof(int) * rows * cols);
     return res;
 }
+
+/***********************************************************
+@description: generator identity matrix float
+@param {int} order
+@return {*}
+************************************************************/
+MatrixFloat *identity_matrix_float(int order)
+{
+    // check order, order must be great than 0
+    if (order <= 0)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return NULL;
+    }
+    // malloc res
+    MatrixFloat *res = NULL;
+    res = (MatrixFloat *)malloc(sizeof(MatrixFloat));
+    if (res == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    res->rows = (size_t)order;
+    res->cols = (size_t)order;
+    // malloc res data
+    res->data = (float *)malloc(sizeof(float) * order * order);
+    if (res->data == NULL)
+    {
+        free(res);
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    // init res->data with 0
+    memset(res->data, 0, sizeof(float) * order * order);
+    // diag must be 1
+    for (size_t i = 0; i < order; i++)
+    {
+        res->data[i * order + i] = 1.0f;
+    }
+    return res;
+}
+
+/***********************************************************
+@description: generator diag matrix float
+@param {float} *data
+@param {size_t} len
+@return {*}
+************************************************************/
+MatrixFloat *diag_matrix_float(float *data, size_t len)
+{
+    // check data and len
+    if (data == NULL || len < 0)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return NULL;
+    }
+    // malloc res
+    MatrixFloat *res = NULL;
+    res = (MatrixFloat *)malloc(sizeof(MatrixFloat));
+    if (res == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    res->rows = len;
+    res->cols = len;
+    // malloc res data
+    res->data = (float *)malloc(sizeof(float) * len * len);
+    if (res->data == NULL)
+    {
+        free(res);
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    // init res->data with 0
+    memset(res->data, 0, sizeof(float) * len * len);
+    // diag must be 1
+    for (size_t i = 0; i < len; i++)
+    {
+        res->data[i * len + i] = data[i];
+    }
+    return res;
+}
+
+/***********************************************************
+@description: generator eye matrix float
+@param {int} rows
+@param {int} cols
+@return {*}
+************************************************************/
+MatrixFloat *eye_matrix_float(int rows, int cols)
+{
+    // check rows and cols
+    if (rows <= 0 || cols <= 0)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return NULL;
+    }
+    // calc eye matrix diag len
+    int col = (rows >= cols ? cols : rows);
+    // malloc res
+    MatrixFloat *res = NULL;
+    res = (MatrixFloat *)malloc(sizeof(MatrixFloat));
+    if (res == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    res->rows = rows;
+    res->cols = cols;
+    // malloc res data
+    res->data = (float *)malloc(sizeof(float) * rows * cols);
+    if (res->data == NULL)
+    {
+        free(res);
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    // init res->data with 0
+    memset(res->data, 0, sizeof(float) * rows * cols);
+    for (int i = 0; i < col; i++)
+    {
+        res->data[i * cols + i] = 1.0f;
+    }
+    return res;
+}
+
+/***********************************************************
+@description: generator zero matrix float
+@param {int} rows
+@param {int} cols
+@return {*}
+************************************************************/
+MatrixFloat *zero_matrix_float(int rows, int cols)
+{
+    // check rows and cols
+    if (rows <= 0 || cols <= 0)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return NULL;
+    }
+    // malloc res
+    MatrixFloat *res = NULL;
+    res = (MatrixFloat *)malloc(sizeof(MatrixFloat));
+    if (res == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    res->rows = rows;
+    res->cols = cols;
+    // malloc res data
+    res->data = (float *)malloc(sizeof(float) * rows * cols);
+    if (res->data == NULL)
+    {
+        free(res);
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    // init res->data with 0
+    memset(res->data, 0, sizeof(float) * rows * cols);
+    return res;
+}
