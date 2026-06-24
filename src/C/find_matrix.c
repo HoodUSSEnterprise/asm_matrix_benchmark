e8b9aa5c19bcf44dd67117f81f33f7c111447e82
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-24 14:40:44 +0800

    feat: Update LastEditTime and enhance find_elem_double function

--- src/C/find_matrix.c
+++ src/C/find_matrix.c
@@ -2,7 +2,7 @@
 @Author: HoodUSSEnterprise
 @Date: 2026-06-17 10:35:02
 @LastEditors: HoodUSSEnterprise
-@LastEditTime: 2026-06-17 13:13:50
+@LastEditTime: 2026-06-24 14:38:44
 @FilePath: \asm_matrix_benchmark\src\C\find_matrix.c
 @Description: find elem position in matrix m
 *************************************************************/
@@ -34,6 +34,42 @@ bool find_elem_int(MatrixInt *m, int elem, Point *pos)
         for (size_t j = 0; j < m->cols; j++)
         {
             if (m->data[i * m->cols + j] == elem)
+            {
+                pos->x = i;
+                pos->y = j;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+/***********************************************************
+@description: find elem position in matrix m
+@param {MatrixDouble} *m
+@param {double} elem
+@param {Point} *pos
+@return {*}
+*************************************************************/
+bool find_elem_double(MatrixDouble *m, double elem, Point *pos)
+{
+    // check m
+    if (m == NULL)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return false;
+    }
+    // check m->data
+    if (m->data == NULL)
+    {
+        fprintf(stderr, "Invalid param!\n");
+        return false;
+    }
+    for (size_t i = 0; i < m->rows; i++)
+    {
+        for (size_t j = 0; j < m->cols; j++)
+        {
+            if (fabs(m->data[i * m->cols + j] - elem) < 1e-6)
             {
                 pos->x = i;
                 pos->y = j;
