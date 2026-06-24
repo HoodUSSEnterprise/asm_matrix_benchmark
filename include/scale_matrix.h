9dd28395421d4f528f06549775373ca8ecbe78a5
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-24 08:49:13 +0800

    feat: Update scale_matrix.h with double matrix scale function

--- include/scale_matrix.h
+++ include/scale_matrix.h
@@ -2,7 +2,7 @@
 @Author: HoodUSSEnterprise
 @Date: 2026-06-16 18:44:04
 @LastEditors: HoodUSSEnterprise
-@LastEditTime: 2026-06-16 18:44:41
+@LastEditTime: 2026-06-23 21:01:24
 @FilePath: \asm_matrix_benchmark\include\scale_matrix.h
 @Description: scale matrix head file
 *************************************************************/
@@ -18,5 +18,13 @@
 @return {*}
  ************************************************************/
 MatrixInt *scale_matrix_int(MatrixInt *m, int scale);
+
+/***********************************************************
+@description: scale matrix double function
+@param {MatrixDouble} *m
+@param {double} scale
+@return {*}
+*************************************************************/
+MatrixDouble *scale_matrix_double(MatrixDouble *m, double scale);
 
 #endif // SCALE_MATRIX_H
