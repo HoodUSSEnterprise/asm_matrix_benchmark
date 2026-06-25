cb818adab16d67ad38252306465d0a510986d86e
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-25 16:01:15 +0800

    style: Add detailed comments to replace_matrix_float_by_coord (Linux)

--- src/assembly/linux/replace_matrix_float_by_coord.asm
+++ src/assembly/linux/replace_matrix_float_by_coord.asm
@@ -20,6 +20,7 @@ section .text
 ; rdi = m, rsi = pos, xmm0 = new_data (System V)
 replace_matrix_float_by_coord:
 
+    ; save callee_register
     push rbx
     push r12
     push r13
@@ -33,6 +34,7 @@ replace_matrix_float_by_coord:
     mov r15, rsi
     movss xmm14, xmm0
 
+    ; check m and pos
     test r14, r14
     jz null_ptr
     test r15, r15
@@ -40,28 +42,37 @@ replace_matrix_float_by_coord:
 
     mov r14, [rdi]
 
+    ; check m->data
     test r14, r14
     jz null_ptr
 
+    ; restore r14
     mov r14, rdi
 
     mov r8, [r15]
     mov r9, [r15 + 8]
 
+    ; check x and y range
+    ; x and y must greater than 0
     cmp r8, 0
     jl index_out_of_range
     cmp r9, 0
     jl index_out_of_range
 
+    ; x and y must in matrix size
+    ; x needs to less than m->cols(no equal)
     cmp r8, [r14 + 16]
     jg index_out_of_range
+    ; y needs to less than m->rows(no equal)
     cmp r9, [r14 + 8]
     jg index_out_of_range
 
+    ; calc index of array
     mov r10, [r14 + 16]
     imul r10, r8
     add r10, r9
 
+    ; change value
     mov r8, [r14]
     movss [r8 + r10 * 4], xmm14
     mov rax, 1
@@ -83,6 +94,7 @@ cleanup:
 
     movss xmm14, [rsp + 36]
     add rsp, 40
+    ; restore callee_register
     pop r15
     pop r14
     pop r13
