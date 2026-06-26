d86ae6a0386fde3b91d6edddff2132f06278db89
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-26 09:26:37 +0800

    fix: correct import path for Matrix

--- example/matrix_int.py
+++ example/matrix_int.py
@@ -2,17 +2,17 @@
 # @Author: HoodUSSEnterprise
 # @Date: 2026-06-22 13:00:31
 # @LastEditors: HoodUSSEnterprise
-# @LastEditTime: 2026-06-22 22:35:45
+# @LastEditTime: 2026-06-25 20:12:41
 # @FilePath: \asm_matrix_benchmark\example\matrix_int.py
 # @Description: example of matrix
 ###########################################################
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 from typing import Union, List
 
-from src.Python import Matrix
+from Python import Matrix
 
 data: List[Union[int, float]] = [i for i in range(1, 13)]
 matrix1 = Matrix(3, 4, data)
