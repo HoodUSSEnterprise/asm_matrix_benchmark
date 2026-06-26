e28dd455e17ed52b59096495aa77c63b8bc1d9d7
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-26 15:38:07 +0800

    refactor: move linux random_matrix_float.asm to float/ subdir

--- src/assembly/linux/float/random_matrix_float.asm
+++ src/assembly/linux/float/random_matrix_float.asm
@@ -1,11 +1,11 @@
-;-------------------------------------------------------------
+; -------------------------------------------------------------
 ; @Author: HoodUSSEnterprise
 ; @Date: 2026-06-24 20:41:55
 ; @LastEditors: HoodUSSEnterprise
-; @LastEditTime: 2026-06-24 20:42:28
-; @FilePath: \asm_matrix_benchmark\src\assembly\linux\random_matrix_float.asm
+; @LastEditTime: 2026-06-26 15:17:45
+; @FilePath: \asm_matrix_benchmark\src\assembly\linux\float\random_matrix_float.asm
 ; @Description: random matrix float nasm code on linux
-;-------------------------------------------------------------
+; -------------------------------------------------------------
 
 global random_matrix_float
 
@@ -17,9 +17,9 @@ extern rand
 extern time
 
 section .rodata
-    malloc_failed db "Memory allocation failed", 0
-    invalid_param db "Invalid param!", 0
-    rand_max dd 0x4f000000
+    malloc_failed  db  "Memory allocation failed", 0
+    invalid_param  db  "Invalid param!", 0
+    rand_max       dd  0x4f000000
 
 section .text
 
@@ -27,6 +27,7 @@ section .text
 ; MatrixFloat *random_matrix_float(size_t rows, size_t cols, float *range, size_t size);
 ; rdi = rows, rsi = cols, rdx = range, rcx = size (System V)
 ; ---------------------------------------------------------------------------------------------
+
 random_matrix_float:
 
     ; save callee_register
@@ -41,10 +42,10 @@ random_matrix_float:
     movss [rsp + 44], xmm13
     movss [rsp + 48], xmm14
 
-    mov r14, rdi ; r14 = rows
-    mov r15, rsi ; r15 = cols
-    mov r12, rdx ; r12 = range
-    mov r13, rcx ; r13 = size
+    mov r14, rdi                        ; r14 = rows
+    mov r15, rsi                        ; r15 = cols
+    mov r12, rdx                        ; r12 = range
+    mov r13, rcx                        ; r13 = size
 
     ; check param rows
     test r14, r14
@@ -62,8 +63,8 @@ random_matrix_float:
     jmp case_size_else
 
 case_size_zero:
-    mov dword [rsp + 32], 0x41200000
-    mov dword [rsp + 36], 0
+    mov dword[rsp + 32], 0x41200000
+    mov dword[rsp + 36], 0
     jmp boundary_done
 
 case_size_one:
@@ -74,7 +75,7 @@ case_size_one:
 
     ; range[0] > 0
     movss [rsp + 32], xmm0
-    mov dword [rsp + 36], 0
+    mov dword[rsp + 36], 0
     jmp boundary_done
 
 range_le_zero:
@@ -82,13 +83,13 @@ range_le_zero:
     jb range_neg
 
     ; range[0] == 0
-    mov dword [rsp + 32], 0x41200000 ; max = 10.0f
-    mov dword [rsp + 36], 0           ; min = 0.0f
+    mov dword[rsp + 32], 0x41200000     ; max = 10.0f
+    mov dword[rsp + 36], 0              ; min = 0.0f
     jmp boundary_done
 
 range_neg:
     ; range[0] < 0
-    mov dword [rsp + 32], 0
+    mov dword[rsp + 32], 0
     movss [rsp + 36], xmm0
     jmp boundary_done
 
@@ -99,8 +100,8 @@ case_size_else:
     jae first_bigger
 
     ; range[0] < range[1]
-    movss [rsp + 32], xmm1 ; max = range[1]
-    movss [rsp + 36], xmm0 ; min = range[0]
+    movss [rsp + 32], xmm1              ; max = range[1]
+    movss [rsp + 36], xmm0              ; min = range[0]
     jmp boundary_done
 
 first_bigger:
@@ -124,37 +125,37 @@ boundary_done:
 
     ; total elements = rows * cols
     mov rdi, r14
-    imul rdi, r15 ; rdi = rows * cols
-    mov r12, rdi  ; preserve element count
-    shl rdi, 2    ; rdi = byte size (sizeof(float) = 4)
+    imul rdi, r15                       ; rdi = rows * cols
+    mov r12, rdi                        ; preserve element count
+    shl rdi, 2                          ; rdi = byte size (sizeof(float) = 4)
     ; malloc res->data
     call malloc wrt ..plt
     test rax, rax
     jz malloc_fail_data
 
     ; init res fields
-    mov [rbx], rax      ; res->data = new malloc data
-    mov [rbx + 8], r14  ; res->rows = rows
-    mov [rbx + 16], r15 ; res->cols = cols
+    mov [rbx], rax                      ; res->data = new malloc data
+    mov [rbx + 8], r14                  ; res->rows = rows
+    mov [rbx + 16], r15                 ; res->cols = cols
 
     ; fill data with random float values
-    mov r15, [rbx]      ; r15 = res->data
+    mov r15, [rbx]                      ; r15 = res->data
     movss xmm14, [rsp + 32]
     movss xmm13, [rsp + 36]
     subss xmm14, xmm13
     movss xmm12, [rel rand_max]
 
-    xor r14, r14        ; i = 0
+    xor r14, r14                        ; i = 0
 
 fill_loop:
-    cmp r14, r12        ; i < total elements
+    cmp r14, r12                        ; i < total elements
     jge end
 
     call rand wrt ..plt
-    cvtsi2ss xmm0, eax  ; xmm0 = (float)rand()
-    divss xmm0, xmm12   ; xmm0 = (float)rand() / RAND_MAX  [0, 1)
-    mulss xmm0, xmm14   ; xmm0 *= (max - min)
-    addss xmm0, xmm13   ; xmm0 += min
+    cvtsi2ss xmm0, eax                  ; xmm0 = (float)rand()
+    divss xmm0, xmm12                   ; xmm0 = (float)rand() / RAND_MAX  [0, 1)
+    mulss xmm0, xmm14                   ; xmm0 *= (max - min)
+    addss xmm0, xmm13                   ; xmm0 += min
 
     movss [r15 + r14 * 4], xmm0
     inc r14
