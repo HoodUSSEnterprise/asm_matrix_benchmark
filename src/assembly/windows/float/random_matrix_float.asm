e4aa496b8aa97e675f784bc436a9486fb535460f
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-26 15:39:25 +0800

    refactor: move windows random_matrix_float.asm to float/ subdir

---
+++ src/assembly/windows/float/random_matrix_float.asm
@@ -0,0 +1,201 @@
+; -------------------------------------------------------------
+; @Author: HoodUSSEnterprise
+; @Date: 2026-06-24 18:52:55
+; @LastEditors: HoodUSSEnterprise
+; @LastEditTime: 2026-06-26 15:21:59
+; @FilePath: \asm_matrix_benchmark\src\assembly\windows\float\random_matrix_float.asm
+; @Description:  random matrix float nasm code on windows
+; -------------------------------------------------------------
+
+global random_matrix_float
+
+extern malloc
+extern free
+extern puts
+extern srand
+extern rand
+extern time
+
+section .rodata
+    malloc_failed  db  "Memory allocation failed", 0    ; malloc failed msg
+    invalid_param  db  "Invalid param!", 0              ; invalid param msg
+    rand_max       dd  32767.0                          ; RAND_MAX as float
+
+section .text
+
+; ---------------------------------------------------------------------------------------------
+; MatrixFloat *random_matrix_float(size_t rows, size_t cols, float *range, size_t size);
+; rcx = rows, rdx = cols, r8 = range, r9 = size
+; ---------------------------------------------------------------------------------------------
+
+random_matrix_float:
+
+    ; save callee_register
+    push rbx
+    push rdi
+    push r12
+    push r13
+    push r14
+    push r15
+    sub rsp, 56
+
+    movss [rsp + 40], xmm12
+    movss [rsp + 44], xmm13
+    movss [rsp + 48], xmm14
+
+    mov r14, rcx                        ; r14 = rows
+    mov r15, rdx                        ; r15 = cols
+    mov r12, r8                         ; r12 = range
+    mov r13, r9                         ; r13 = size
+
+    ; check param rows
+    test r14, r14
+    jz null_ptr
+    ; check param cols
+    test r15, r15
+    jz null_ptr
+
+    ; determine max_boundary and min_boundary
+    ; store on stack: [rsp + 32] = max (float), [rsp + 36] = min (float)
+    cmp r13, 0
+    je case_size_zero
+    cmp r13, 1
+    je case_size_one
+    jmp case_size_else
+
+case_size_zero:
+    mov dword[rsp + 32], 0x41200000     ; max = 10.0f
+    mov dword[rsp + 36], 0              ; min = 0.0f
+    jmp boundary_done
+
+case_size_one:
+    movss xmm0, [r12]                   ; xmm0 = range[0]
+    xorps xmm1, xmm1                    ; xmm1 = 0.0
+    comiss xmm0, xmm1
+    jbe range_le_zero                   ; if range[0] <= 0
+
+    ; range[0] > 0
+    movss [rsp + 32], xmm0              ; max = range[0]
+    mov dword[rsp + 36], 0              ; min = 0.0f
+    jmp boundary_done
+
+range_le_zero:
+    comiss xmm0, xmm1
+    jb range_neg                        ; if range[0] < 0
+
+    ; range[0] == 0
+    mov dword[rsp + 32], 0x41200000     ; max = 10.0f
+    mov dword[rsp + 36], 0              ; min = 0.0f
+    jmp boundary_done
+
+range_neg:
+    ; range[0] < 0
+    mov dword[rsp + 32], 0              ; max = 0.0f
+    movss [rsp + 36], xmm0              ; min = range[0]
+    jmp boundary_done
+
+case_size_else:
+    movss xmm0, [r12]                   ; xmm0 = range[0]
+    movss xmm1, [r12 + 4]               ; xmm1 = range[1]
+    comiss xmm0, xmm1
+    jae first_bigger
+
+    ; range[0] < range[1]
+    movss [rsp + 32], xmm1              ; max = range[1]
+    movss [rsp + 36], xmm0              ; min = range[0]
+    jmp boundary_done
+
+first_bigger:
+    movss [rsp + 32], xmm0              ; max = range[0]
+    movss [rsp + 36], xmm1              ; min = range[1]
+
+boundary_done:
+    ; srand((unsigned)time(NULL))
+    xor rcx, rcx
+    call time
+    mov ecx, eax
+    call srand
+
+    ; malloc res struct (24 bytes)
+    mov ecx, 24
+    call malloc
+    test rax, rax
+    jz malloc_fail_struct
+
+    mov rbx, rax                        ; rbx = res
+
+    ; total elements = rows * cols
+    mov rdi, r14
+    imul rdi, r15                       ; rdi = rows * cols
+
+    ; malloc res->data
+    lea rcx, [rdi * 4]
+    call malloc
+    test rax, rax
+    jz malloc_fail_data
+
+    ; init res fields
+    mov [rbx], rax                      ; res->data = new malloc data
+    mov [rbx + 8], r14                  ; res->rows = rows
+    mov [rbx + 16], r15                 ; res->cols = cols
+
+    ; fill data with random float values
+    mov r15, [rbx]                      ; r15 = res->data
+    movss xmm14, [rsp + 32]             ; xmm14 = max_boundary
+    movss xmm13, [rsp + 36]             ; xmm13 = min_boundary
+    subss xmm14, xmm13                  ; xmm14 = max - min
+    movss xmm12, [rel rand_max]         ; xmm12 = RAND_MAX
+
+    xor r14, r14                        ; i = 0
+
+fill_loop:
+    cmp r14, rdi                        ; i < total elements
+    jge end
+
+    call rand
+    cvtsi2ss xmm0, eax                  ; xmm0 = (float)rand()
+    divss xmm0, xmm12                   ; xmm0 = (float)rand() / RAND_MAX  [0, 1)
+    mulss xmm0, xmm14                   ; xmm0 *= (max - min)
+    addss xmm0, xmm13                   ; xmm0 += min
+
+    movss [r15 + r14 * 4], xmm0         ; res->data[i] = value
+    inc r14
+    jmp fill_loop
+
+malloc_fail_struct:
+    lea rcx, [rel malloc_failed]
+    call puts
+    mov rax, 0
+    jmp cleanup
+
+malloc_fail_data:
+    lea rcx, [rel malloc_failed]
+    call puts
+    mov rcx, rbx
+    call free
+    mov rax, 0
+    jmp cleanup
+
+null_ptr:
+    lea rcx, [rel invalid_param]
+    call puts
+    mov rax, 0                          ; return NULL
+    jmp cleanup
+
+end:
+    mov rax, rbx
+
+cleanup:
+
+    movss xmm14, [rsp + 48]
+    movss xmm13, [rsp + 44]
+    movss xmm12, [rsp + 40]
+    add rsp, 56                         ; restore stack pointer
+    ; restore callee_register
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rdi
+    pop rbx
+    ret
