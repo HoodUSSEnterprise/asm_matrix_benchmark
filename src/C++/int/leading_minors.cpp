fdf702235074ef2ab62d1366f7b396db5b0ddfd0
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-26 09:24:13 +0800

    style: add detailed comments to leading_minors (int)

---
+++ src/C++/int/leading_minors.cpp
@@ -0,0 +1,42 @@
+/************************************************************
+@Author: HoodUSSEnterprise
+@Date: 2026-06-23 17:39:26
+@LastEditors: HoodUSSEnterprise
+@LastEditTime: 2026-06-26 08:56:48
+@FilePath: \asm_matrix_benchmark\src\C++\int\leading_minors.cpp
+@Description: leading principal minors c++ code
+*************************************************************/
+
+#include "matrix_int_cpp.h"
+#include <stdexcept>
+
+/***********************************************************
+@description: Computes all leading principal minors of the matrix.
+@return {*}
+*************************************************************/
+std::vector<MatrixInt> MatrixInt::leading_minors() const
+{
+    if (rows != cols)
+    {
+        throw std::invalid_argument("leading_minors: square matrix required");
+    }
+
+    std::vector<MatrixInt> result;
+    result.reserve(rows);
+
+    // Extract leading principal submatrices of order 1..rows
+    // Each submatrix is the top-left corner of the original matrix
+    for (size_t order = 1; order <= rows; order++)
+    {
+        MatrixInt sub(order, order);
+        for (size_t i = 0; i < order; i++)
+        {
+            for (size_t j = 0; j < order; j++)
+            {
+                sub(i, j) = data[i * cols + j];
+            }
+        }
+        result.push_back(std::move(sub));
+    }
+    return result;
+}
