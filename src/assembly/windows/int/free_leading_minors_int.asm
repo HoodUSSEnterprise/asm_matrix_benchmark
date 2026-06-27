e0102c18c32335af0a3ccca20069e2f88be6fdce
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-27 09:10:31 +0800

    feat: enhance free_leading_minors_int to handle null pointers and prevent dangling references

--- src/assembly/windows/int/free_leading_minors_int.asm
+++ src/assembly/windows/int/free_leading_minors_int.asm
@@ -2,20 +2,22 @@
 ; @Author: HoodUSSEnterprise
 ; @Date: 2026-06-26 19:23:17
 ; @LastEditors: HoodUSSEnterprise
-; @LastEditTime: 2026-06-26 19:23:23
+; @LastEditTime: 2026-06-27 09:09:48
 ; @FilePath: \asm_matrix_benchmark\src\assembly\windows\int\free_leading_minors_int.asm
 ; @Description: free leading minors int nasm code
 ; -------------------------------------------------------------
 global free_leading_minors_int
 
-extern printf
+extern puts
 extern puts
 
 section .rodata
-
-section .data
+    invalid_param  db  "Invalid param!", 0    ; invalid param msg
 
 section .text
+
+; void free_leading_minors_int(Leading_Minors_Int **l);
+; rcx = l
 
 free_leading_minors_int:
 
@@ -27,6 +29,58 @@ free_leading_minors_int:
     push r14
     push r15
     sub rsp, 32                         ; allocate shadow space for printf
+
+    ; check l
+    mov r14, rcx                        ; r14 = l
+    test r14, r14
+    jz null_ptr
+
+    ; check *l
+    mov r14, [rcx]                      ; r14 = *l
+    test r14, r14
+    jz null_ptr
+
+    ; init loop, free Leading_Minors_Int->matrix_data;
+    ; typedef struct Leading_Minors_Int
+    ; {
+    ;     MatrixInt *matrix_data;
+    ;     size_t len;
+    ; } Leading_Minors_Int;
+    xor rdi, rdi                        ; i = 0
+    mov rsi, [r14 + 8]                  ; rsi = len
+    mov r15, [r14]                      ; r15 = (*l)->matrix_data
+
+loop1:
+    cmp rdi, rsi                        ; i < len ?
+    jae next
+
+    ; r15 = matrix_data
+    mov rax, rdi                        ; rax = i
+    imul rax, 24                        ; rax *= 24
+    mov rcx, [r15 + rax]                ; rcx = (*l)->matrix_data[i].data
+    call free
+
+    inc rdi
+    jmp loop1
+
+next:
+    ; free (*l)->matrix_data
+    mov rcx, r15
+    call free
+
+    ; free *l
+    mov rcx, r14
+    call free
+
+    ; optional, *m = NULL
+    ; To avoid dangling pointers, I assign NULL here
+    mov r14, 0
+    jmp cleanup
+
+null_ptr:
+    lea rcx, [rel invalid_param]        ; rcx = invalid_param
+    call puts
+    jmp cleanup
 
 cleanup:
     add rsp, 32                         ; restore stack pointer
