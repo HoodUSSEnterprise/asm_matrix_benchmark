da28a61279600719068c15da8f76286b12fa3273
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-27 10:10:39 +0800

    fix:relocation R_X86_64_PC32 against symbol `free@@GLIBC_2.2.5' can not be used when making a PIE object

--- src/assembly/linux/double/free_leading_minors_double.asm
+++ src/assembly/linux/double/free_leading_minors_double.asm
@@ -58,7 +58,7 @@ loop1:
     mov rax, r12                        ; rax = i
     imul rax, 24                        ; rax *= 24
     mov rdi, [r15 + rax]                ; rcx = (*l)->matrix_data[i].data
-    call free
+    call free wrt ..plt
 
     inc r12
     jmp loop1
@@ -66,11 +66,11 @@ loop1:
 next:
     ; free (*l)->matrix_data
     mov rdi, r15
-    call free
+    call free wrt ..plt
 
     ; free *l
     mov rdi, r14
-    call free
+    call free wrt ..plt
 
     ; optional, *m = NULL
     ; To avoid dangling pointers, I assign NULL here
@@ -79,7 +79,7 @@ next:
 
 null_ptr:
     lea rdi, [rel invalid_param]        ; rcx = invalid_param
-    call puts
+    call puts wrt ..plt
     jmp cleanup
 
 cleanup:
