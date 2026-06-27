f9e3e3d362673cc31f3471a7479d16c236ca6cd4
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-27 19:20:55 +0800

    fix: Fix data processing and dimensionality checking in the reversible matrix function

--- src/Python/inv_matrix.py
+++ src/Python/inv_matrix.py
@@ -2,63 +2,79 @@
 # @Author: HoodUSSEnterprise
 # @Date: 2026-06-25 16:46:58
 # @LastEditors: HoodUSSEnterprise
-# @LastEditTime: 2026-06-26 9:40:40
+# @LastEditTime: 2026-06-27 19:19:42
 # @FilePath: \asm_matrix_benchmark\src\Python\inv_matrix.py
 # @Description: invertible matrix python code
 ###########################################################
 from math import fabs
-from typing import Any, Union
+
+from typing import Union, List
 
 from .base_matrix import Matrix
+from .rank_matrix import rank
+
 
 ###########################################################
 # @description: invertible matrix
 # @param {Matrix} m
 # @return {*}
 ###########################################################
-def inv_matrix(m:Matrix) -> Matrix | None:
-    
+def inv_matrix(m: Matrix) -> Matrix | None:
+
     # check dimension
     if m.rows != m.cols:
         print("Invalid param")
         return None
-    
-    aug_matrix = [[0 for _ in range( 2 * m.cols)] for _ in range(m.rows)]
+
+    if rank(m) != m.rows:
+        print("It's not a invertible matrix")
+        return None
+
+    aug_matrix = [[0.0 for _ in range(2 * m.cols)] for _ in range(m.rows)]
     # init aug matrix
     for i in range(m.rows):
-        for j in range(2*m.cols):
+        for j in range(2 * m.cols):
             if j < m.cols:
-                aug_matrix[i][j] = m.data[i][j]
+                aug_matrix[i][j] = m.data[i * m.cols + j]
             else:
                 aug_matrix[i][j] = 1.0 if j - i == m.rows else 0.0
-    
+
     # use gauss elimination
     row = 0
     col = 0
     while row < m.rows and col < m.cols:
         # find main element
         pivot = row
-        while pivot < m.rows and fabs(m.data[pivot][col]) < 1e-6:
+        while pivot < m.rows and fabs(m.data[pivot * m.cols + col]) < 1e-6:
             pivot += 1
-        
+
         # that means this column is zero of all
         if pivot == m.rows:
             continue
-        
+
         # exchange lines
         if pivot != row:
             aug_matrix[row], aug_matrix[pivot] = aug_matrix[pivot], aug_matrix[row]
-        
+
         # normalization
         pivot_val = aug_matrix[row][col]
         for j in range(2 * m.cols):
             aug_matrix[row][j] /= pivot_val
-        
+
         # elimination this line
         for i in range(m.rows):
-            factor = aug_matrix.data[i][col] / aug_matrix.data[row][col]
-            for j in range(cols, 2 * m.cols):
+            if i == row:
+                continue
+
+            factor = aug_matrix[i][col] / aug_matrix[row][col]
+            for j in range(col, 2 * m.cols):
                 aug_matrix[i][j] -= factor * aug_matrix[row][j]
 
+    # extract res
+    res_data: List[Union[int, float]] = [0 for _ in range(m.rows * m.cols)]
 
-                
+    for i in range(m.rows):
+        for j in range(m.cols):
+            res_data[i * m.cols + j] = aug_matrix[i][j]
+
+    return Matrix(m.rows, m.cols, res_data)
