e9fdc898516775ce444b119af04503850ce0111c
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-27 09:50:49 +0800

    feat: add free_matrix_float implementation for memory management

---
+++ src/assembly/windows/float/free_matrix_float.asm
@@ -0,0 +1,86 @@
+; -------------------------------------------------------------
+; @Author: HoodUSSEnterprise
+; @Date: 2026-06-27 09:20:31
+; @LastEditors: HoodUSSEnterprise
+; @LastEditTime: 2026-06-27 09:46:48
+; @FilePath: \asm_matrix_benchmark\src\assembly\windows\float\free_matrix_float.asm
+; @Description: free matrix float nasm code
+; -------------------------------------------------------------
+global free_matrix_float
+
+extern free
+extern puts
+
+section .rodata
+    invalid_param  db  "Invalid param!", 0    ; invalid param msg
+
+section .text
+
+free_matrix_float:
+
+    ; save callee_register
+    push rbx
+    push rdi
+    push r12
+    push r13
+    push r14
+    push r15
+    sub rsp, 32                         ; allocate shadow space for printf
+
+cleanup:
+    add rsp, 32                         ; restore stack pointer
+    ; restore callee_register
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rdi
+    pop rbx
+    ret
+    ; save callee_register
+    push rbx
+    push rdi
+    push r12
+    push r13
+    push r14
+    push r15
+    sub rsp, 32                         ; allocate shadow space for printf
+
+    ; check m
+    mov r14, rcx                        ; r14 = m
+    test r14, r14
+    jz null_ptr
+
+    ; check *m
+    mov r14, [rcx]                      ; r14 = *m
+    test r14, r14
+    jz null_ptr
+
+    ; free (*m)->data
+    mov rcx, [r14]                      ; rcx = (*m)->data
+    call free
+
+    ; free *m
+    mov rcx, r14                        ; rcx = *m
+    call free
+
+    ; optional, *m = NULL
+    ; To avoid dangling pointers, I assign NULL here
+    mov r14, 0
+    jmp cleanup
+
+null_ptr:
+    lea rcx, [rel invalid_param]        ; rcx = invalid_param
+    call puts
+    jmp cleanup
+
+cleanup:
+    add rsp, 32                         ; restore stack pointer
+    ; restore callee_register
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rdi
+    pop rbx
+    ret
