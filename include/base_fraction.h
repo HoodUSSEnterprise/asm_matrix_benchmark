a5004efb61f7774225afe7e00e366e9a78eab8e2
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-28 09:10:59 +0800

    feat: Add Fraction struct definition

---
+++ include/base_fraction.h
@@ -0,0 +1,20 @@
+/************************************************************
+@Author: HoodUSSEnterprise
+@Date: 2026-06-28 08:55:20
+@LastEditors: HoodUSSEnterprise
+@LastEditTime: 2026-06-28 09:01:55
+@FilePath: \asm_matrix_benchmark\include\base_fraction.h
+@Description: add struct of fraction
+*************************************************************/
+#ifndef BASE_FRACTION_H
+#define BASE_FRACTION_H
+
+#include <stdio.h>
+
+typedef struct Fraction
+{
+    int x;
+    int y;
+} Fraction;
+
+#endif // BASE_FRACTION_H
