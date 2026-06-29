62d0373a8799b0e65c2920cc08994b372cc1bc8f
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-29 09:37:06 +0800

    fix: preserve rsi as callee-saved register in find_matrix_double

--- src/assembly/windows/double/find_matrix_double.asm
+++ src/assembly/windows/double/find_matrix_double.asm
@@ -26,6 +26,7 @@ find_elem_double:
     ; save callee_register
     push rbx
     push rdi
+    push rsi
     push r12
     push r13
     push r14
@@ -141,6 +142,7 @@ cleanup:
     pop r14
     pop r13
     pop r12
+    pop rsi
     pop rdi
     pop rbx
     ret
