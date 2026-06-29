4e07fbf9ea73848e651abe1d79cae50a1d2e64cf
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-29 13:55:54 +0800

    feat: update inverse matrix implementation

--- src/Python/inv_matrix.py
+++ src/Python/inv_matrix.py
@@ -2,7 +2,7 @@
 # @Author: HoodUSSEnterprise
 # @Date: 2026-06-25 16:46:58
 # @LastEditors: HoodUSSEnterprise
-# @LastEditTime: 2026-06-27 19:19:42
+# @LastEditTime: 2026-06-29 13:13:05
 # @FilePath: \asm_matrix_benchmark\src\Python\inv_matrix.py
 # @Description: invertible matrix python code
 ###########################################################
@@ -45,11 +45,12 @@ def inv_matrix(m: Matrix) -> Matrix | None:
     while row < m.rows and col < m.cols:
         # find main element
         pivot = row
-        while pivot < m.rows and fabs(m.data[pivot * m.cols + col]) < 1e-6:
+        while pivot < m.rows and fabs(aug_matrix[pivot][col]) < 1e-6:
             pivot += 1
 
         # that means this column is zero of all
         if pivot == m.rows:
+            col += 1
             continue
 
         # exchange lines
@@ -70,11 +71,13 @@ def inv_matrix(m: Matrix) -> Matrix | None:
             for j in range(col, 2 * m.cols):
                 aug_matrix[i][j] -= factor * aug_matrix[row][j]
 
+        row += 1
+
     # extract res
     res_data: List[Union[int, float]] = [0 for _ in range(m.rows * m.cols)]
 
     for i in range(m.rows):
         for j in range(m.cols):
-            res_data[i * m.cols + j] = aug_matrix[i][j]
+            res_data[i * m.cols + j] = aug_matrix[i][j + m.cols]
 
     return Matrix(m.rows, m.cols, res_data)
