47168f8f02adddc837669c45ad6289ef050b33c0
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-29 09:37:12 +0800

    fix: preserve rsi as callee-saved register in find_matrix_float

--- src/assembly/windows/float/find_matrix_float.asm
+++ src/assembly/windows/float/find_matrix_float.asm
@@ -27,6 +27,7 @@ find_elem_float:
     ; save callee_register
     push rbx
     push rdi
+    push rsi
     push r12
     push r13
     push r14
@@ -141,6 +142,7 @@ cleanup:
     pop r14
     pop r13
     pop r12
+    pop rsi
     pop rdi
     pop rbx
     ret
