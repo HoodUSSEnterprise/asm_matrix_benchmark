587c5a0636374d557c78800435a57f5882b0e5da
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-29 09:37:07 +0800

    fix: preserve rsi as callee-saved register in mul_matrix_double

--- src/assembly/windows/double/mul_matrix_double.asm
+++ src/assembly/windows/double/mul_matrix_double.asm
@@ -27,6 +27,7 @@ mul_matrix_double:
     ; save callee_register
     push rbx
     push rdi
+    push rsi
     push r12
     push r13
     push r14
@@ -187,6 +188,7 @@ cleanup:
     pop r14
     pop r13
     pop r12
+    pop rsi
     pop rdi
     pop rbx
     ret
