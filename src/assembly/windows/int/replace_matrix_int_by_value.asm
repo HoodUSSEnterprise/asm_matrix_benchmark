08b5fcc6141b82c6794d18458568e1d18639d0c4
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-29 09:37:21 +0800

    fix: preserve rsi as callee-saved register in replace_matrix_int_by_value

--- src/assembly/windows/int/replace_matrix_int_by_value.asm
+++ src/assembly/windows/int/replace_matrix_int_by_value.asm
@@ -26,6 +26,7 @@ replace_matrix_int_by_value:
     ; save callee_register
     push rbx
     push rdi
+    push rsi
     push r12
     push r13
     push r14
@@ -102,6 +103,7 @@ cleanup:
     pop r14
     pop r13
     pop r12
+    pop rsi
     pop rdi
     pop rbx
     ret
