fb5e8e347e787add45cc9ab0093f02820f8ca4f0
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-29 09:37:11 +0800

    fix: preserve rsi as callee-saved register in compare_matrix_float

--- src/assembly/windows/float/compare_matrix_float.asm
+++ src/assembly/windows/float/compare_matrix_float.asm
@@ -28,6 +28,7 @@ is_equal_matrix_float:
     ; save callee_register
     push rbx
     push rdi
+    push rsi
     push r12
     push r13
     push r14
@@ -124,6 +125,7 @@ cleanup:
     pop r14
     pop r13
     pop r12
+    pop rsi
     pop rdi
     pop rbx
     ret
