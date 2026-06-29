2a087a5941cb93d9a0650dfa5a3bcf6818aad8e1
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-29 09:37:06 +0800

    fix: preserve rsi as callee-saved register in extract_diag_matrix_double

--- src/assembly/windows/double/extract_diag_matrix_double.asm
+++ src/assembly/windows/double/extract_diag_matrix_double.asm
@@ -28,6 +28,7 @@ extract_diag_double:
     ; save callee_register
     push rbx
     push rdi
+    push rsi
     push r12
     push r13
     push r14
@@ -114,6 +115,7 @@ cleanup:
     pop r14
     pop r13
     pop r12
+    pop rsi
     pop rdi
     pop rbx
     ret
