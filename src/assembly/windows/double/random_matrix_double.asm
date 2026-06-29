5fefd335d689068de512ec8578824a68f29a10d0
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-29 09:37:07 +0800

    fix: preserve rsi as callee-saved register in random_matrix_double

--- src/assembly/windows/double/random_matrix_double.asm
+++ src/assembly/windows/double/random_matrix_double.asm
@@ -36,6 +36,7 @@ random_matrix_double:
     ; save callee_register
     push rbx
     push rdi
+    push rsi
     push r12
     push r13
     push r14
@@ -200,6 +201,7 @@ cleanup:
     pop r14
     pop r13
     pop r12
+    pop rsi
     pop rdi
     pop rbx
     ret
